b8ff0993376285913dd9dad90ab35dd5832b4f30
shan alvi <shanalvi438@gmail.com>
2023-08-03 11:27:02 +0500

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'activecampaign/api-php' => array(
+            'pretty_version' => 'v2.0.3',
+            'version' => '2.0.3.0',
+            'reference' => '2bf5b72ba8668de8dcb27e194948028276e748c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../activecampaign/api-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
